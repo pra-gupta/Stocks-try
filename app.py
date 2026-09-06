e59662ec464a7890054a43d1a87871b16c989173
pra-gupta <107258443+pra-gupta@users.noreply.github.com>
2026-09-07 00:08:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -113,7 +113,7 @@ if not df_master.empty:
             csv_mcap = float(row.get('MarketCapCSV', 0))
             
             base_symbol, candidate_tickers = generate_ticker_candidates(raw_symbol, company_name)
-            
+            print(str(candidate_tickers))
             t = None
             hist_recent = pd.DataFrame()
             resolved_ticker = None
